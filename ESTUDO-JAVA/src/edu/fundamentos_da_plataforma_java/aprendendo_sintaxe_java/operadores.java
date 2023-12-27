c250e5a21282dfd0ca1b69f7ac002ef9225558c0
victorino0071 <victor.emmanuel@aluno.ufr.edu.br>
2023-12-27 12:21:24 -0400

    Aplicação de alguns operadores

--- ESTUDO-JAVA/src/edu/fundamentos_da_plataforma_java/aprendendo_sintaxe_java/operadores.java
+++ ESTUDO-JAVA/src/edu/fundamentos_da_plataforma_java/aprendendo_sintaxe_java/operadores.java
@@ -8,6 +8,30 @@ public class operadores {
 
         System.out.println(nomeCompleto);
 
+        int numero = 5;
+        numero = - numero;
+
+        System.out.println( - numero);
+        System.out.println( numero);
+        
+        //x repetição
+        numero = numero * -1;
+        //numero = numero + 1 é igual a numero++;
+        numero ++;
+
+
+        System.out.println(numero);
+
+        //Condição usando "? e :"
+        int a, b;
+        a = 6;
+        b = 6;
+
+        //É o mesmo que usar "if" e "else"
+        String resultado = a==b ? "verdadeiro" : "falso";
+
+        System.out.println(resultado);
+
 
 
     }
