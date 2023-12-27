5287f90583323985115d78949bf5946800868c85
victorino0071 <victor.emmanuel@aluno.ufr.edu.br>
2023-12-27 11:30:35 -0400

    Começo do Estudo de operadores

---
+++ ESTUDO-JAVA/src/edu/fundamentos_da_plataforma_java/aprendendo_sintaxe_java/operadores.java
@@ -0,0 +1,15 @@
+package edu.fundamentos_da_plataforma_java.aprendendo_sintaxe_java;
+
+public class operadores {
+    public static void main (String [] args){
+
+        
+        String nomeCompleto = "Victor " + "Emmanuel";
+
+        System.out.println(nomeCompleto);
+
+
+
+    }
+    
+}
